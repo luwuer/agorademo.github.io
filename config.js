20f68b976a4f647c78b5fed5d9e156fe81808a84
luwuer <html6@foxmail.com>
2020-12-28 10:33:12 +0800

    update

---
+++ config.js
@@ -0,0 +1,9 @@
+window.config = {
+  agoraDefault: {
+    appid: "2470cbbf82bc4afda9cbd0ae8041ec24",
+    token:
+      "0062470cbbf82bc4afda9cbd0ae8041ec24IADII5l9JfaR90H/wrgC0ESd+iORaMSLIiKpuWahEubY7c6w9mwAAAAAEADkOavBHjzoXwEAAQAdPOhf",
+    channel: "1226"
+  },
+  logLevel: "all" // one of ['off', 'error', 'warn', 'info', 'all']
+};
